48d62b46da56d0cf38bb7f557a21cd96c49bdad0
Harold Putman <h.putman@ieee.org>
2022-05-07 18:31:22 -0400

    updates for 2022

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # twin-spires-spier
-Pull Horse names from Churchill Downs site
+
+Pull Horse names from a website
 
 ## Description
 
-Every year I copy and paste horse names from the site to a spreasheet. Automating it this year.
+Use information from a public website to populate [a spreadsheet](./KY%20Derby%20Blank.xlsx).
+with horse names.
+
+## Notes
+
+* The equibase format page changes during race day. When horses are scratched you lose information about their pole position, jockey, etc...
+* This doesn't yet handle combined entries (like 1 and 1A) In that case we probably should put both names into the same column, since those are treated as the same horse.
+
+## Disclaimer
+
+The software here is intended to be used for collaborative analysis of a certain horse
+race the first Saturday in May. Any gambling or wagering activity should be done only within the confines of Federal, State, and local laws. 
--- index.js
+++ index.js
@@ -1,61 +1,100 @@
 const puppeteer = require('puppeteer');
+const { data } = require('cheerio/lib/api/attributes');
+const { replaceWith } = require('cheerio/lib/api/manipulation');
 
-const churchillDownsURL = 'https://www.churchilldowns.com/racing-wagering/toteboard';
-const horseRacingNationURL = 'https://entries.horseracingnation.com/entries-results/churchill-downs';
-
-const date = '2021-05-06';
-
-async function churchilldowns(page, date) {
-  let race = 1;
-  let races = 0;
-  const results = [];
-  await page.goto(`https://www.churchilldowns.com/racing-wagering/toteboard#/${date}/${race}`)
-  do {
-    if (!races) {
-      races = await page.$$eval('.shortcuts a', links => { return links.length });
-    }
-    await page.click(`.shortcuts a:nth-child(${race})`);
-    const horses = await page.$$eval('table.race-table tbody tr', rows => { return rows.map(row => {
-      return {
-        number: row.querySelector('td:nth-child(2)').textContent,
-        name: row.querySelector('td:nth-child(3)').textContent
+/**
+ * Scrape horse information for a particular race
+ * @param {Page} page The puppeteer page.
+ * @param {number} raceNumber The race number
+ * @returns An array of data about each horse.
+ *   * pole - The pole position
+ *   * horse - The horse name
+ *   * origin - The origin of the horse like KY or IRE
+ *   * jockey - Name of the Jockey
+ *   * trainer - Name of the trainer
+ */
+async function equibaseRace(page, raceNumber) {
+  const data = await page.$eval(`#RACE${raceNumber} ~ .c-entries-data table tbody`, (el) => {
+    return Array.from(el.querySelectorAll('tr')).map(row => {
+      const cols = row.querySelectorAll('td'); 
+      if (row.classList.contains('scratch')) {
+        // Scratched horse don't have enough information..
+        return null;
+      }
+      if (cols.length > 10) {
+        const jockeyColumn = cols.length == 12 ? 7 : 6;
+        const matches = /([^(]+)(?: \((.*)\))/.exec(cols[2].innerText);
+        const horse = matches[1];
+        const origin =  matches[2];
+        return {
+          pole: cols[0].innerText,
+          horse,
+          origin,
+          jockey: cols[jockeyColumn].innerText,
+          trainer: cols[jockeyColumn + 2].innerText
         };
-      });
-    });
-    results.push(horses);
-    race += 1;
-  } while (race <= races);
-  return results;
+      } else {
+        return null;
+      }
+    }).filter(row => !!row);
+  });
+  return data;
 }
 
-async function horseracingnation(page, date) {
-  const results = [];
-  await page.goto(`${horseRacingNationURL}/${date}`);
-  const raceData = await page.$$('.my-5');
-  for (let i = 0; i < raceData.length; i++) {
-    const horses = await raceData[i].$$eval('.table-entries tbody tr', (rows) => {
-      return rows.map((row) => {
-        return {
-          number: row.querySelector('td:nth-child(2)').textContent.trim(),
-          name: row.querySelector('h4').textContent
-        }
-      });
+/**
+ * Scrape the "equibase" format page on website. This is
+ * like a text-only race program.
+ * @param {Page} page The puppeteer page.
+ * @param {String} date The race date in mm/dd/yyyy format
+ * @returns {Array} Data for each race
+ */
+async function equibase(page, date) {
+  const data = [];
+  try {
+    await page.goto(`http://equibase.churchilldowns.com/eqbRaceEntriesDisplay.cfm?TRK=CD&CY=USA&DATE=${date}&STYLE=CD`);
+    const links = await page.$eval('.race-nav', (el) => {
+      return Array.from(el.querySelectorAll('a')).map((e) => e.getAttribute('href'));
     });
-    results.push(horses);
+    const raceCount = links.length - 1;
+    for (let i = 1; i <= raceCount; i++) {
+      const race = await equibaseRace(page, i);
+      data.push(race);
+    }
+  } catch(e) {
+    console.error(e);
   }
-  return results;
+  return data;
+}
+
+/**
+ * Generates a CSV listing horses by pole position. Each
+ * row has an array of horse names, one for each race.
+ * 1: ["horse1.1", "horse2.1", "horse3.1"]
+ * @param {Array} raceData Data about all the races.
+ */
+function generateCSV(raceData) {
+  const poles = {};
+  const headers = ['number'];
+  headers.push( ...[...Array(raceData.length).keys()].map(val => `race${val + 1}`));
+  console.log(headers.join(','));
+  raceData.forEach((data, index) => {
+    data.forEach(pos => {
+      if (typeof poles[pos.pole] === 'undefined') {
+        poles[pos.pole] = Array(raceData.length);
+      } 
+      poles[pos.pole][index] = pos.horse;
+    });
+  });
+  Object.keys(poles).forEach(pole => {
+    console.log(`${pole},` + poles[pole].join(','));
+  });
 }
 
 (async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  const results = await horseracingnation(page, date);
-  // const results = await churchilldowns(page, date);
-  console.log('race,number,name');
-  results.forEach((horses, racenum) => {
-    horses.forEach(horse => {
-      console.log(`${racenum + 1},${horse.number},${horse.name}`);
-    })
-  });
+  const date = '05/07/2022';
+  const results = await equibase(page, date);
+  generateCSV(results);
   await browser.close()
-})()
+})();
